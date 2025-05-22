14761b764124963ca2ed11ee74b754069eda95dd
ftgreat <ftgreat@163.com>
2025-05-22 15:20:06 +0800

    Refactor dev-v0.1 rm flagscale.
    Signed-off-by: ftgreat <ftgreat@163.com>

--- flagscale/flagscale/compress/combined_algo.py
+++
@@ -1,9 +0,0 @@
-import copy
-import os
-import pdb
-import sys
-
-
-def prepare_compress_methods(compress_cfg):
-    recipes = copy.deepcopy(compress_cfg)
-    return recipes
--- flagscale/tests/unit_tests/compressor/test_compressor.py
+++
@@ -1,7 +0,0 @@
-from flagscale.compress.compressor import compress, prepare_config
-
-
-def test_config():
-    test_config_path = "test_config.yaml"
-    cfg = prepare_config()
-    compress(cfg)
