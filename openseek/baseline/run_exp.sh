8220ba7fa67ece0dbe785f4561d87c256a786cc5
Marverlises <1963835770@qq.com>
2025-05-27 14:26:08 +0800

    Add baseline setup and run

--- openseek/baseline/run_exp.sh
+++ openseek/baseline/run_exp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+CURRENT_DIR=$(pwd)
+
+export CUDA_DEVICE_MAX_CONNECTIONS=1
 
 if [ $# -eq 0 ]; then
    echo "$0 {start|stop}"
@@ -11,32 +14,32 @@ if [ "$2" == "llama" ]; then
 	case $1 in
 		start)
 		echo "Start a traing"
-		python run.py --config-path ./examples/llama/conf --config-name config
+		python $CURRENT_DIR/FlagScale/run.py --config-path $CURRENT_DIR/configs/OpenSeek-Small-v1-Baseline --config-name config_deepseek_v3_1_4b.yaml
 		;;
 		profile)
 		echo "Start a profiling"
-		nsys profile --wait=all -s none -t nvtx,cuda -o ./build/profile_aquila --force-overwrite true --capture-range=cudaProfilerApi --capture-range-end=stop \
-		python run.py --config-path ./examples/llama/conf --config-name config
+		nsys profile --wait=all -s none -t nvtx,cuda -o $CURRENT_DIR/build/profile_aquila --force-overwrite true --capture-range=cudaProfilerApi --capture-range-end=stop \
+		python $CURRENT_DIR/FlagScale/run.py --config-path $CURRENT_DIR/examples/llama/conf --config-name config
 		;;
 		stop)
 		echo "Stop a traing"
-		python run.py --config-path ./examples/llama/conf --config-name config action=stop
+		python $CURRENT_DIR/FlagScale/run.py --config-path $CURRENT_DIR/examples/llama/conf --config-name config action=stop
 		;;
 	esac
 else
 	case $1 in
 		start)
-		echo "Start a traing"
-		python3 run.py --config-path configs/OpenSeek-Small-v1-Baseline --config-name $2
+		echo "Start a traing in 1"
+		python3 $CURRENT_DIR/FlagScale/run.py --config-path $CURRENT_DIR/configs/OpenSeek-Small-v1-Baseline --config-name config_deepseek_v3_1_4b.yaml
 		;;
 		profile)
 		echo "Start a profiling"
-		nsys profile --wait=all -s none -t nvtx,cuda -o ./build/profile_aquila --force-overwrite true --capture-range=cudaProfilerApi --capture-range-end=stop \
-		python3 run.py --config-path configs --config-name $2
+		nsys profile --wait=all -s none -t nvtx,cuda -o $CURRENT_DIR/build/profile_aquila --force-overwrite true --capture-range=cudaProfilerApi --capture-range-end=stop \
+		python3 $CURRENT_DIR/FlagScale/run.py --config-path configs --config-name $2
 		;;
 		stop)
 		echo "Stop a traing"
-		python3 run.py --config-path configs --config-name $2 action=stop
+		python3 $CURRENT_DIR/FlagScale/run.py --config-path configs --config-name $2 action=stop
 		;;
 	esac
 fi
